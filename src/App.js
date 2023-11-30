c09a4388434a634e8d3689dfed74d9cbf434cb7f
vimalpksh <itisvimal@gmail.com>
2023-11-30 23:28:58 +0530

    Home and new post component created
    working in progress for the other components.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import About from "./About";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -11,42 +12,44 @@ import PostPage from "./PostPage";
 import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "My First Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Made a video about Tesla Q1 results",
+    },
+    {
+      id: 2,
+      title: "My 2nd Post",
+      datetime: "July 02, 2021 11:17:36 AM",
+      body: "Made a video about Assasins",
+    },
+    {
+      id: 3,
+      title: "My 3rd Post",
+      datetime: "July 03, 2021 11:17:36 AM",
+      body: "Made a video about GTA",
+    },
+    {
+      id: 4,
+      title: "My 4th Post",
+      datetime: "July 04, 2021 11:17:36 AM",
+      body: "Made a video about WATCH DOGS",
+    },
+  ]);
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
   return (
     <div className="App">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/newpost">New Post</Link>
-          </li>
-          <li>
-            <Link to="/postpage">Post Page</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/newpost" element={<NewPost />} />
-        <Route path="/postpage" element={<PostLayout />}>
-          <Route index element={<PostPage />} />
-          <Route path=":id" element={<Post />} />
-          <Route path="newpost" element={<NewPost />} />
-        </Route>
-      </Routes>
-      {/* <Header />
-      <Nav />
-      <Home />
+      <Header title="MySpace Social Media" />
+      <Nav search={search} setSearch={setSearch} />
+      <Home posts={posts} />
       <NewPost />
       <PostPage />
       <About />
       <Missing />
-      <Footer /> */}
+      <Footer />
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
-const Post = () => {
-  const { id } = useParams();
+const Post = ({ post }) => {
   return (
-    <div>
-      <br></br>Post {id}
-    </div>
+    <article className="post">
+      <h2>{post.title}</h2>
+      <p className="postDate">{post.datetime}</p>
+      <p className="postBody">
+        {post.body.length <= 25 ? post.body : `${post.body.slice(0, 25)}...`}
+      </p>
+    </article>
   );
 };
 
