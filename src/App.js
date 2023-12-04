932849c9f4f7437d340b8da27b99fc2ed53026cb
vimalpksh <itisvimal@gmail.com>
2023-12-04 23:11:27 +0530

    axios server implemented

--- src/App.js
+++ src/App.js
@@ -11,39 +11,34 @@ import PostLayout from "./PostLayout";
 import PostPage from "./PostPage";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import { format } from "date-fns";
+import api from "./api/Posts";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "My First Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Made a video about Tesla Q1 results",
-    },
-    {
-      id: 2,
-      title: "My 2nd Post",
-      datetime: "July 02, 2021 11:17:36 AM",
-      body: "Made a video about Assasins",
-    },
-    {
-      id: 3,
-      title: "My 3rd Post",
-      datetime: "July 03, 2021 11:17:36 AM",
-      body: "Made a video about GTA",
-    },
-    {
-      id: 4,
-      title: "My 4th Post",
-      datetime: "July 04, 2021 11:17:36 AM",
-      body: "Made a video about WATCH DOGS",
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [postTitle, setPostTitle] = useState("");
   const [postBody, setPostBody] = useState(" ");
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await api.get("/posts");
+        setPosts(response.data);
+      } catch (err) {
+        if (err.response) {
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else {
+          console.log(`Eror: ${err.message}`);
+        }
+      }
+    };
+
+    fetchItems();
+  }, []);
 
   useEffect(() => {
     const filteredResults = posts.filter((post) => {
