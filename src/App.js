3832f67bbf5c9c1a9ee3bf04b91df01070759cb7
vimalpksh <itisvimal@gmail.com>
2023-12-05 16:48:39 +0530

    custom axios fetch hook implemented

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { format } from "date-fns";
 import api from "./api/Posts";
 import EditPost from "./EditPost";
 import useWindowSize from "./hooks/useWindowSize";
+import useAxiosFetch from "./hooks/useAxiosFetch";
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -25,25 +26,13 @@ function App() {
   const [editBody, setEditBody] = useState(" ");
   const navigate = useNavigate();
   const { width } = useWindowSize();
+  const { data, fetchError, isLoading } = useAxiosFetch(
+    "http://localhost:3500/posts"
+  );
 
   useEffect(() => {
-    const fetchItems = async () => {
-      try {
-        const response = await api.get("/posts");
-        setPosts(response.data);
-      } catch (err) {
-        if (err.response) {
-          console.log(err.response.data);
-          console.log(err.response.status);
-          console.log(err.response.headers);
-        } else {
-          console.log(`Eror: ${err.message}`);
-        }
-      }
-    };
-
-    fetchItems();
-  }, []);
+    setPosts(data);
+  }, [data]);
 
   useEffect(() => {
     const filteredResults = posts.filter((post) => {
@@ -115,7 +104,12 @@ function App() {
       <Header title="MySpace Social Media" width={width} />
       <Nav search={search} setSearch={setSearch} />
       <Routes>
-        <Route path="/" element={<Home posts={posts} />} />
+        <Route
+          path="/"
+          element={
+            <Home posts={posts} fetchError={fetchError} isLoading={isLoading} />
+          }
+        />
         <Route path="post">
           <Route
             index
